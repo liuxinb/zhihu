cd636ad1580325c2bdd2a9dc68a8f5110aff6114
liuxina <1321415620@qq.com>
2018-03-15 10:34:53 +0800

    后台干净的模板

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,79 @@
+@extends('admin.layout')
+
+@section('title', '后台首页')
+
+@section('head')
+Zhi Hu 后台首页
+@endsection
+
+@section('content')
+
+            <div class="tpl-content-scope">
+                <div class="note note-info">
+                    <h3>知乎，发现更大的世界
+                        <span class="close" data-close="note"></span>
+                    </h3>
+                     <p> welcome !</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="am-u-lg-3 am-u-md-6 am-u-sm-12">
+                    <div class="dashboard-stat blue">
+                        <div class="visual">
+                            <i class="am-icon-comments-o"></i>
+                        </div>
+                        <div class="details">
+                            <div class="number"> 1349 </div>
+                            <div class="desc"> 新消息 </div>
+                        </div>
+                        <a class="more" href="#"> 查看更多
+                    <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+                    </div>
+                </div>
+                <div class="am-u-lg-3 am-u-md-6 am-u-sm-12">
+                    <div class="dashboard-stat red">
+                        <div class="visual">
+                            <i class="am-icon-bar-chart-o"></i>
+                        </div>
+                        <div class="details">
+                            <div class="number"> 62% </div>
+                            <div class="desc"> 收视率 </div>
+                        </div>
+                        <a class="more" href="#"> 查看更多
+                    <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+                    </div>
+                </div>
+                <div class="am-u-lg-3 am-u-md-6 am-u-sm-12">
+                    <div class="dashboard-stat green">
+                        <div class="visual">
+                            <i class="am-icon-apple"></i>
+                        </div>
+                        <div class="details">
+                            <div class="number"> 653 </div>
+                            <div class="desc"> 苹果设备 </div>
+                        </div>
+                        <a class="more" href="#"> 查看更多
+                    <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+                    </div>
+                </div>
+                <div class="am-u-lg-3 am-u-md-6 am-u-sm-12">
+                    <div class="dashboard-stat purple">
+                        <div class="visual">
+                            <i class="am-icon-android"></i>
+                        </div>
+                        <div class="details">
+                            <div class="number"> 786 </div>
+                            <div class="desc"> 安卓设备 </div>
+                        </div>
+                        <a class="more" href="#"> 查看更多
+                    <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+                    </div>
+                </div>
+        </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,38 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*---------test模板------------*/
+Route::get('admin/index', function () {
+    return view('admin.index');
+});
+
+//login登陆
+Route::get('admin/login', function () {
+    return view('admin.login');
+});
+
+//table表格
+Route::get('admin/table', function () {
+    return view('admin.table');
+});
+
+//form表单
+Route::get('admin/amazeui', function () {
+    return view('admin.amazeui');
+});
+
+//form表单2
+Route::get('admin/line', function () {
+    return view('admin.line');
+});
+
+//图片表格
+Route::get('admin/list', function () {
+    return view('admin.list');
+});
+
+    /*---------后台功能------------*/
+
+    // Route::resource('users', 'App\Http\Controllers\admin\usersController');
+    // Route::get('users', 'userController@index');
